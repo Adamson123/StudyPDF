e4da1125b2dbea12b39dae868aceedd2563bb321
Ajibade Adam <111712386+Adamson123@users.noreply.github.com>
2025-06-28 17:53:10 +0100

    Update Sidebar.tsx

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -8,7 +8,7 @@ const LeftSection = ({ showSidebar }: { showSidebar: boolean }) => {
       style={{
         scrollbarColor: "hsl(var(--border)) transparent",
       }}
-      className={`fixed bottom-0 left-0 top-0 z-40 w-[700px] overflow-x-auto overflow-y-auto bg-background pt-16 shadow-[4px_0px_3px_rgba(0,0,0,0.3)] transition-all ${showSidebar ? "translate-x-0" : "-translate-x-full"}`}
+      className={`fixed bottom-0 left-0 top-0 z-40 max-w-screen md:max-w-[700px] overflow-x-auto overflow-y-auto bg-background pt-16 shadow-[4px_0px_3px_rgba(0,0,0,0.3)] transition-all ${showSidebar ? "translate-x-0" : "-translate-x-full"}`}
     >
       <div className="flex flex-col gap-5">
         <div className="border- flex flex-col pb-2">
